84a6132768275f16146b1016222480abc3761a26
ponderingponderpond <wichtelkaiser@protonmail.com>
2019-10-20 20:40:04 +0200

    Broke up code into modules and added required importing functionality.

---
+++ monsterbattle_battle.py
@@ -0,0 +1,48 @@
+#Global Variable, just to illustrate how those work, really.
+from random import randint #importing the specific means you can just call randint without using random.randint everytime
+from random import choice
+
+import monsterbattle_monsterpool as mbtl_mpool #importing the module monsterbattle_monsterpool and renaming it to mbtl_mpool to work with it a little easier
+
+damageDone = 3 
+
+def monsterBattle():  #this defines a function
+    #Building the variable contents for the monster based on the selection in "monsterPool"
+    selectedMonster = choice(mbtl_mpool.monsterPool)
+    selectedMonsterName = selectedMonster.get('name')
+    selectedMonsterArticle = selectedMonster.get('article')
+    selectedMonsterHealth = selectedMonster.get('health')
+    #Printing multiline text using ''' Stuff ''''
+    print(f'''
+    You have encountered {selectedMonsterArticle} {selectedMonsterName}. Get ready to fight. It has {selectedMonsterHealth} health.
+    Do you posess the will to fight this {selectedMonsterName}, {playerName}? 
+    Roll the dice and learn your fate!
+    ''') #the "f" allows to use {} to call on variables instead of needing to go the long way via + [] +
+    playerMoraleThrowCheck = input()
+    if playerMoraleThrowCheck.lower() == 'roll': #the .lower() converts any input like ROLL to roll, so the input is valid since it expects lower case "roll"
+        playerMoraleThrow = randint(1, 6)
+        if playerMoraleThrow > 1:
+            print('You have the will to fight and engage the enemy!')
+        else:
+            print('You lack the will to fight.')
+    else:
+        print('All you had to do was type \"roll\"...')
+    
+    global damageDone #overrides the global variable for both local and global scope
+    damageDone = 0 #local variable because it's within a defined function
+    
+    while selectedMonsterHealth > 0:
+        if playerMoraleThrow > 1:
+            actualDamageDealt = randint(1, 5) #putting the randint() in the "while" loop continually generates new numbers instead of just repeating the one that was picked outside of it (as I had it before).
+            damageDone += actualDamageDealt #+= does the same thing as damageDone = damageDone + standardDot, since damageDone is used both as a variable and a value. This is an "augmented assignment operator"
+            selectedMonsterHealth -= actualDamageDealt
+            print(f'You hit the {selectedMonsterName} for {actualDamageDealt} reducing its health to {selectedMonsterHealth}')
+            if selectedMonsterHealth <= 0:
+                print(f'You have dealt {damageDone} damage overall.') 
+                cheers = ['Huzza!', 'Glorious!', 'Fantastic!', 'You can do better, though.', 'Honestly, it wasn\'t that hard.', 'Gee golly, aren\'t you strong...']
+                print(f'You have killed the {selectedMonsterName}! {choice(cheers)}')
+                break
+        else:
+            print(f'You are freigthened by the {selectedMonsterName}. Best you run away and save your petty hide!')
+            break #stops the while loop, so it doesn't infinetely print to terminal that you are a coward.
+
--- monsterbattle_main.py
+++ monsterbattle_main.py
@@ -2,60 +2,17 @@
 from random import randint #importing the specific means you can just call randint without using random.randint everytime
 from random import choice
 
-#monsterPool - these are dictonaries with several 'key': value pairs
-m_skeletonWarrior = {'health': 20, 'name': 'Skeleton Warrior', 'article': 'a'}
-m_orcWarrior = {'health': 27, 'name': 'Orc Warrior', 'article': 'an'}
-m_wrigglingFinger = {'health': 3, 'name': 'Wriggling Finger', 'article': 'a'}
-
-monsterPool = [m_orcWarrior, m_skeletonWarrior, m_wrigglingFinger]
+import monsterbattle_battle as mbtl_btl #Not sure if I use from if I can pass on values generated in this module to the imported one. 
 
 #locationPool - unused so far
 l_forrestPath = {'combatPlayerBonus': 1}
 
-#Global Variable, just to illustrate how those work, really.
-damageDone = 3 
-
-def monsterBattle():  #this defines a function
-    #Building the variable contents for the monster based on the selection in "monsterPool"
-    selectedMonster = choice(monsterPool)
-    selectedMonsterName = selectedMonster.get('name')
-    selectedMonsterArticle = selectedMonster.get('article')
-    selectedMonsterHealth = selectedMonster.get('health')
-    print(f'You have encountered {selectedMonsterArticle} {selectedMonsterName}. Get ready to fight. It has {selectedMonsterHealth} health.') #the "f" allows to use {} to call on variables instead of needing to go the long way via + [] +
-    print(f'Do you posess the will to fight this {selectedMonsterName}, {playerName}? Roll the dice and learn your fate!')
-    playerMoraleThrowCheck = input()
-    if playerMoraleThrowCheck.lower() == 'roll': #the .lower() converts any input like ROLL to roll, so the input is valid since it expects lower case "roll"
-        playerMoraleThrow = randint(1, 6)
-        if playerMoraleThrow > 1:
-            print('You have the will to fight and engage the enemy!')
-        else:
-            print('You lack the will to fight.')
-    else:
-        print('All you had to do was type \"roll\"...')
-    
-    global damageDone #overrides the global variable for both local and global scope
-    damageDone = 0 #local variable because it's within a defined function
-    
-    while selectedMonsterHealth > 0:
-        if playerMoraleThrow > 1:
-            actualDamageDealt = randint(1, 5) #putting the randint() in the "while" loop continually generates new numbers instead of just repeating the one that was picked outside of it (as I had it before).
-            damageDone += actualDamageDealt #+= does the same thing as damageDone = damageDone + standardDot, since damageDone is used both as a variable and a value. This is an "augmented assignment operator"
-            selectedMonsterHealth -= actualDamageDealt
-            print(f'You hit the {selectedMonsterName} for {actualDamageDealt} reducing its health to {selectedMonsterHealth}')
-            if selectedMonsterHealth <= 0:
-                print(f'You have dealt {damageDone} damage overall.') 
-                cheers = ['Huzza!', 'Glorious!', 'Fantastic!', 'You can do better, though.', 'Honestly, it wasn\'t that hard.', 'Gee golly, aren\'t you strong...']
-                print(f'You have killed the {selectedMonsterName}! {choice(cheers)}')
-        else:
-            print(f'You are freigthened by the {selectedMonsterName}. Best you run away and save your petty hide!')
-            break #stops the while loop, so it doesn't infinetely print to terminal that you are a coward.
-
-
 print('Who are you?')
 playerName = input()
+mbtl_btl.playerName = playerName #Here we define "PlayerName" to be the same for the module monsterbattle_battle as well, otherwise using the mbtl_btl.monsterBattle() later won't know what the player typed as their name.
 if len(playerName) > 3:
     print(f'{playerName}, eh? How odd. Well, {playerName}, would you like to go on an adventure?')
-elif len(playerName) < 3:
+elif len(playerName) <= 3:
     print(f'{playerName}? That\'s kind of short, isn\'t it? Fine. Want to have an adventure?')
 
 goForth = input()
@@ -66,10 +23,10 @@ if goForth.lower() == 'yes':
         playerDiceRoll = randint(1, 6)
         print ('You have rolled a', playerDiceRoll, '.')
         if playerDiceRoll >= 2:  
-            print(f'You hear something suspicious. Do you wish to investigate, {playerName}?')
+            print('You hear something suspicious. Do you wish to investigate?')
             playerChoice = input()
             if playerChoice.lower() == 'yes':           
-                monsterBattle() #this calls/executes the function
+                mbtl_btl.monsterBattle() #this calls/executes the function from the module mbtl_btl which we shortened above from monsterbattle_battle
             else:
                 print(f'{playerName} is a little coward, eh?')
         else:
