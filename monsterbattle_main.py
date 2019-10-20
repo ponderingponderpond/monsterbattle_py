f7373f0c1ae6337b2afb5d539fbd1f3e4456d472
ponderingponderpond <wichtelkaiser@protonmail.com>
2019-10-20 17:15:21 +0200

    Cleaned up print statements.

--- monsterbattle_main.py
+++ monsterbattle_main.py
@@ -21,19 +21,15 @@ def monsterBattle():  #this defines a function
     selectedMonsterName = selectedMonster.get('name')
     selectedMonsterArticle = selectedMonster.get('article')
     selectedMonsterHealth = selectedMonster.get('health')
-
-    monsterEncounteredMsg = f'You have encountered {selectedMonsterArticle} {selectedMonsterName}. Get ready to fight. It has {selectedMonsterHealth} health.' #the "f" allows to use {} to call on variables instead of needing to go the long way via + [] +
-    print(monsterEncounteredMsg)
-    
-    playerMoraleCheckMsg = f'Do you posess the will to fight this {selectedMonsterName}, {playerName}? Roll the dice and learn your fate!'
-    print(playerMoraleCheckMsg)
+    print(f'You have encountered {selectedMonsterArticle} {selectedMonsterName}. Get ready to fight. It has {selectedMonsterHealth} health.') #the "f" allows to use {} to call on variables instead of needing to go the long way via + [] +
+    print(f'Do you posess the will to fight this {selectedMonsterName}, {playerName}? Roll the dice and learn your fate!')
     playerMoraleThrowCheck = input()
     if playerMoraleThrowCheck == 'roll':
         playerMoraleThrow = randint(1, 6)
         if playerMoraleThrow > 1:
             print('You have the will to fight and engage the enemy!')
         else:
-            print('You lack the will to fight and flee in terror.')
+            print('You lack the will to fight.')
     else:
         print('All you had to do was type \"roll\"...')
     
@@ -46,24 +42,19 @@ def monsterBattle():  #this defines a function
         if playerMoraleThrow > 1:
             damageDone += actualDotDamage #+= does the same thing as damageDone = damageDone + standardDot, since damageDone is used both as a variable and a value. This is an "augmented assignment operator"
             selectedMonsterHealth -= actualDotDamage
-            dmgOutputMsg = f'You hit the {selectedMonsterName} for {actualDotDamage} reducing its health to {selectedMonsterHealth}'
-            print(dmgOutputMsg)
+            print(f'You hit the {selectedMonsterName} for {actualDotDamage} reducing its health to {selectedMonsterHealth}')
             if selectedMonsterHealth <= 0:
-                dmgDoneMsg = f'You have dealt {damageDone} damage overall.'
-                print(dmgDoneMsg) 
+                print(f'You have dealt {damageDone} damage overall.') 
                 cheers = ['Huzza!', 'Glorious!', 'Fantastic!', 'You can do better, though.', 'Honestly, it wasn\'t that hard.', 'Gee golly, aren\'t you strong...']
-                cheersMsg = f'You have killed the {selectedMonsterName}! {choice(cheers)}'
-                print(cheersMsg)
+                print(f'You have killed the {selectedMonsterName}! {choice(cheers)}')
         else:
-            fleeingMsg = f'You are freigthened by the {selectedMonsterName} and will deal {actualDotDamage} damage. Best you run away and save your petty hide!'
-            print(fleeingMsg)
+            print(f'You are freigthened by the {selectedMonsterName}. Best you run away and save your petty hide!')
             break #stops the while loop, so it doesn't infinetely print to terminal that you are a coward.
 
 
 print('Who are you?')
 playerName = input()
-lightlyInsultingIntro = f'{playerName}, eh? How odd. Well, {playerName}, would you like to go on an adventure?'
-print(lightlyInsultingIntro)
+print(f'{playerName}, eh? How odd. Well, {playerName}, would you like to go on an adventure?')
 
 goForth = input()
 if goForth == 'yes':
@@ -73,14 +64,12 @@ if goForth == 'yes':
         playerDiceRoll = randint(1, 6)
         print ('You have rolled a', playerDiceRoll, '.')
         if playerDiceRoll >= 2:  
-            playerHearsSomethingMsg = f'You hear something suspicious. Do you wish to investigate, {playerName}?'
-            print(playerHearsSomethingMsg)
+            print(f'You hear something suspicious. Do you wish to investigate, {playerName}?')
             playerChoice = input()
             if playerChoice == 'yes':           
                 monsterBattle() #this calls/executes the function
             else:
-                playerCowardMsg = f'{playerName} is a little coward, eh?'
-                print(playerCowardMsg)
+                print(f'{playerName} is a little coward, eh?')
         else:
             print('Nothing happens and you have no adventures. Pity.')
     else:
