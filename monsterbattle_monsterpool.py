8cd99268ead119ba8b3acb8b4ae50755e7bdc161
ponderingponderpond <wichtelkaiser@protonmail.com>
2019-11-05 18:32:04 +0100

    Repaired Monster Pool Tuple conflict

--- monsterbattle_monsterpool.py
+++ monsterbattle_monsterpool.py
@@ -1,9 +1,9 @@
 #monsterPool - these are dictonaries with several 'key': value pairs
-m_skeletonWarrior = {'health': 20, 'name': 'Skeleton Warrior', 'article': 'a'},
-m_orcWarrior = {'health': 27, 'name': 'Orc Warrior', 'article': 'an'},
-m_wrigglingFinger = {'health': 3, 'name': 'Wriggling Finger', 'article': 'a'},
-m_strangeHyena = {'health': 15, 'name': 'Strange Hyena', 'article': 'a'},
-m_giantAnt = {'health': 35, 'name': 'Giant Ant', 'article': 'a'},
+m_skeletonWarrior = {'health': 20, 'name': 'Skeleton Warrior', 'article': 'a'}
+m_orcWarrior = {'health': 27, 'name': 'Orc Warrior', 'article': 'an'}
+m_wrigglingFinger = {'health': 3, 'name': 'Wriggling Finger', 'article': 'a'}
+m_strangeHyena = {'health': 15, 'name': 'Strange Hyena', 'article': 'a'}
+m_giantAnt = {'health': 35, 'name': 'Giant Ant', 'article': 'a'}
 m_singingDuck = {'health': 7, 'name': 'Singing Duck', 'article': 'a'}
 
 #This is just a list which can be chosen from. There's probably a sexier way of doing the same thing.1
